962eb71092e08996a1bd93964e361d8d38587c92
neecrobot <131327722+neecrobot@users.noreply.github.com>
2023-05-07 10:51:07 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
+let GY = 0
+let GX = 0
 let X = 0
 let Y = 0
 let A = 0
-let B = 0
+let M = 0
 basic.forever(function () {
-    if (A == 0 && B == 0) {
+    if (A == 0 && M == 0) {
         if (input.buttonIsPressed(Button.A)) {
             A = 1
-            B = 1
+            M = 1
         }
     }
 })
@@ -44,32 +46,34 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (B == 1) {
+    if (M == 1) {
         music.playMelody("C5 B A G F E D C ", 1000)
-        B = 0
-    } else if (B == 2) {
+        M = 0
+    } else if (M == 2) {
         for (let カウンター = 0; カウンター <= 4; カウンター++) {
             music.playMelody("C D E F G A B C5 ", 3000)
-            B = 0
+            M = 0
         }
     }
 })
 basic.forever(function () {
     led.unplot(X, Y)
-    if (input.acceleration(Dimension.X) < -32) {
+    GX = input.acceleration(Dimension.X)
+    GY = input.acceleration(Dimension.Y)
+    if (GX < -32) {
         if (0 < X) {
             X += -1
         }
-    } else if (input.acceleration(Dimension.X) > 32) {
+    } else if (GX > 32) {
         if (X < 4) {
             X += 1
         }
     }
-    if (input.acceleration(Dimension.Y) < -32) {
+    if (GY < -32) {
         if (0 < Y) {
             Y += -1
         }
-    } else if (input.acceleration(Dimension.Y) > 32) {
+    } else if (GY > 32) {
         if (Y < 4) {
             Y += 1
         }
@@ -78,7 +82,7 @@ basic.forever(function () {
     led.plot(X, Y)
     if (A == 2) {
         if (X == 2 && Y == 2) {
-            B = 2
+            M = 2
             basic.showLeds(`
                 . . . . .
                 . # # # .
